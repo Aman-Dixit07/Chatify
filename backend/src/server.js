f03fe8696934ca15dcc696d51d3f6ec9687ee054
Aman Dixit <amandixitbsp@gmail.com>
2025-12-03 23:47:27 +0530

    Implement user signup functionality with validation and password hashing

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 
@@ -11,6 +12,8 @@ dotenv.config();
 const app = express();
 const Port = process.env.Port || 5000;
 
+//middlewares
+app.use(express.json()); //to read the req.body otherwise it will return undefined
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
@@ -23,4 +26,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(Port, () => console.log(`Server running on port- ${Port}`));
+app.listen(Port, () => {
+  console.log(`Server running on port- ${Port}`);
+  connectDB();
+});
