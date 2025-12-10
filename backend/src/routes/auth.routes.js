a6128a91f1e52142512f5196fab0ac1f71a40e38
Aman Dixit <amandixitbsp@gmail.com>
2025-12-10 21:11:01 +0530

    Integrated Arcjet for enhanced security.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -7,13 +7,20 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
+
 router.post("/signup", signup);
-router.post("/login", login);
+router.get("/login", login);
 router.post("/logout", logout);
 
 router.post("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
 
 export default router;
